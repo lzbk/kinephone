81c19611bc781f1df1130cd2ccc03066ec33944f
pitrack <innova@crunchbang-pitrack>
2014-09-03 13:02:51 +0200

    - added font awesome - changed layout

--- js/app/modules/home/controllers/HomeCtrl.js
+++ js/app/modules/home/controllers/HomeCtrl.js
@@ -8,7 +8,7 @@ function HomeCtrl($scope, $http, $interval, $routeParams, Data) {
     $scope.panelVisible = false;
     $scope.currentId = 0; // keep the id of the current selected detail in order to manage right panel opening / closing
     $scope.data = {}; // all datas for the current language and method
-    $scope.details = {}; // details for the current item selected
+    $scope.details = null; // details for the current item selected
     $scope.gender = 'male';
     // url params //
     // method
@@ -24,9 +24,15 @@ function HomeCtrl($scope, $http, $interval, $routeParams, Data) {
         $scope.lang = 1;
     }
 
-    $scope.init = function() {
+    $scope.init = function() {       
         $('img[usemap]').rwdImageMaps();
-        $('#right-panel').hide();
+        var image =  document.getElementById('big-image');
+        var hammer = new Hammer(image);
+        hammer.on("press",function(e){
+            console.log('youpla');
+            $scope.toggleConfigPanel();
+        });
+       
     };
     angular.element(document).ready(function() {
         // get datas
@@ -39,7 +45,6 @@ function HomeCtrl($scope, $http, $interval, $routeParams, Data) {
     function success(e) {
         window.setTimeout(function() {
             $scope.data = e;
-            console.log($scope.data);
             $scope.$apply();
             $scope.init();
         }, 0);
@@ -48,33 +53,41 @@ function HomeCtrl($scope, $http, $interval, $routeParams, Data) {
     function error(e) {
         console.log(e);
     }
+
     $scope.toggleRightPanel = function() {
         $("#wrapper").toggleClass("toggled");
         $scope.panelVisible = !$scope.panelVisible;
     }
+
+    $scope.toggleConfigPanel = function(){
+         $("#config-bar-wrapper").toggleClass("toggled");
+        //$('#config-bar-wrapper').toggle(500);
+    }
+
     $scope.showDetails = function(e) {
         var id = e.target.id;
         // load details
         $scope.details = getDetails(id);
-        // no items have been selected or the item previously selected is the same
-        if ($scope.currentId == 0 || $scope.currentId == id || ($scope.currentId != id && !$scope.panelVisible)) {
-            $scope.toggleRightPanel();
-        }
         $scope.currentId = id;
     }
+
     $scope.play1 = function() {
         // this line make it work on iOS
         $('#player1').get(0).load();
         $('#player1').get(0).play();
     }
+
     $scope.play2 = function() {
         // this line make it work on iOS
         $('#player2').get(0).load();
         $('#player2').get(0).play();
     }
+
     $scope.genderChanged = function(value) {
+        console.log(value);
         $scope.gender = value || 'male';
     }
+    
     $scope.modelChanged = function(value) {
         $scope.methode = value || 'gattegno';
     }
